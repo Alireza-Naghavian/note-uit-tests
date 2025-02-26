b78e99610f2399ffef4855ea2899df19fd1b45d7
alireza <alirezanghngh1379@gmail.com>
2025-02-26 15:27:32 +0330

    initialize note project

---
+++ src/components/NoteHeader.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useNotes } from "../context/NotesContext";
+
+function NoteHeader({sortBy, onSort }) {
+const notes= useNotes()
+  return (
+    <div className="note-header">
+      <h1 style={{display:"none"}} >My Notes ({notes?.length})</h1>
+      <select value={sortBy} onChange={onSort}>
+        <option value="latest">Sort based on latest notes</option>
+        <option value="earliest">Sort based on earliest notes</option>
+        <option value="completed">Sort based on completed notes</option>
+      </select>
+    </div>
+  );
+}
+
+export default NoteHeader;
